f73311f9ad23f2dce0a7b6f22d186816e9349298
Mindaugas <mindaugas.poskus@visma.com>
2021-01-19 17:02:08 +0200

    added server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var port = process.env.PORT || 3000;
+var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+var router = express.Router();
+
+console.log('——————————- Run on port '+ port);
+
+/****************************** Router ***************************/
+router.get('*', function(req, res){
+    res.sendFile('index.html', { root: __dirname + '/' });
+});
+
+/****************************** /Router ***************************/
+
+app.use(express.static(__dirname + '/')); // Static (public) folder
+
+app.use(bodyParser.urlencoded({extended:true}));// get information from html forms
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(methodOverride());
+app.use('/', router); // app.use('/parent', router); call all from localhost:port/parent/*
+
+app.listen(port);
