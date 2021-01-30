add1901a600fbebe0e26df2d2054249d5daa7aeb
Mindaugas <mindaugas.poskus@visma.com>
2021-01-30 06:58:31 +0200

    Addem main menu and navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { AssetsComponent } from './assets/assets.component';
+import { HomeComponent } from './home/home.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { TransactionsComponent } from './transactions/transactions.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'assets', component: AssetsComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'transactions', component: TransactionsComponent},
+  {path: 'portfolio', component: PortfolioComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
+import { AssetsComponent } from './assets/assets.component';
+import { HomeComponent } from './home/home.component';
+import { TransactionsComponent } from './transactions/transactions.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainMenuComponent,
+    AssetsComponent,
+    HomeComponent,
+    TransactionsComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule,
